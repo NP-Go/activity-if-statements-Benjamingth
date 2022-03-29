7c407a24ce3d0fcb234de57f7383c802b239ec27
Benjamingth <benjamin.gth@hotmail.com>
2022-03-29 12:52:22 +0800

    Added if solution

--- if.go
+++ if.go
@@ -1,10 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
 	number1 := 17
 	number2 := 24
 	resultMessage := "No outcome."
 	//Insert your code here
 	//Hint: You may wish to make use of strconv.Itoa to convert int to string
+	if number1 < number2 {
+		fmt.Printf("%v is bigger than %v.", number2, number1)
+		difference := number2 - number1
+		fmt.Printf("\nNumber2 is bigger by %v.", difference)
+	}
 
+	if number1 == number2 {
+		fmt.Println(resultMessage)
+	}
+
+	if number1 > number2 {
+		fmt.Printf("%v is bigger than %v.", number1, number2)
+		difference2 := number1 - number2
+		fmt.Printf("\nNumber1 is bigger by %v.", difference2)
+	}
 }
